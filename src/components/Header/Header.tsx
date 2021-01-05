a9745b2df76c25999eaa57975eea93542192b6eb
Erin Untermeyer <aerinuntermeyer@gmail.com>
2021-01-04 18:20:22 -0700

    Add header with app title and about nav link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import './Header.scss'
 
 const Header: React.FC = () => {
 	return (
-		<div className="Header">
-		</div>
+		<nav>
+			<NavLink to='/'><h1>Erin's Eats</h1></NavLink>
+			<NavLink to='/about'>About</NavLink>
+		</nav>
 	)
 }
 
