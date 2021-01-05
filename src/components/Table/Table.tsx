592356fe14781405bf82cd11addb50c1da493b66
Erin Untermeyer <aerinuntermeyer@gmail.com>
2021-01-04 20:57:36 -0700

    Build Table component to render restaurant table

---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Restaurant } from '../../helpers/definitions'
+import './Table.scss'
+
+interface TableProps {
+	restaurantList: Restaurant[]
+}
+
+const Table: React.FC<TableProps> = (props) => {
+
+	const createRestaurantTable = () => {
+		return props.restaurantList.map((restaurant: Restaurant, index: number) => {
+			const { name, city, state, telephone, genre } = restaurant
+			return (
+				<tr key={index}>
+					<td>{name}</td>
+					<td>{city}</td>
+					<td>{state}</td>
+					<td>{telephone}</td>
+					<td>{genre}</td>
+				</tr>
+			)
+		})
+	}
+
+	return (
+		<div>
+			<h2>Restaurants</h2>
+			<table className="Table">
+				<thead className="TableHead">
+					<tr>
+						<th>Name</th>
+						<th>City</th>
+						<th>State</th>
+						<th>Phone Number</th>
+						<th>Genre(s)</th>
+					</tr>
+				</thead>
+				<tbody>
+					{createRestaurantTable()}
+				</tbody>
+			</table>
+		</div>
+	)
+}
+
+export default Table
