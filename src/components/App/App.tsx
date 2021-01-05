f117f36ed949a1303478db8bb29e61cf3c44dbef
Erin Untermeyer <aerinuntermeyer@gmail.com>
2021-01-04 18:20:44 -0700

    Render Header in App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Header from '../Header/Header'
 import './App.scss'
 
 const App: React.FC = () => {
   return (
     <div className="App">
+			<Header />
     </div>
   )
 }
