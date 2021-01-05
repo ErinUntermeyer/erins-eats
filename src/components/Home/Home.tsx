3482600ebd4224d5f606be1bf03ef7a08a7b0fb6
Erin Untermeyer <aerinuntermeyer@gmail.com>
2021-01-04 20:56:33 -0700

    Build Home component to render Table component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Table from '../Table/Table'
+import { Restaurant } from '../../helpers/definitions'
+import { getRestaurants } from '../../helpers/apiCalls'
 import './Home.scss'
 
-const Home: React.FC = () => {
+const Home = () => {
+	const [ restaurants, setRestaurants ] = useState<Array<Restaurant>>()
+	
+	useEffect(() => {
+		getRestaurants()
+			.then(data => {
+				setRestaurants(data)
+			})
+	}, [])
+
 	return (
 		<div className="Home">
+			{restaurants &&
+				<Table restaurantList={restaurants} />
+			}
 		</div>
 	)
 }
